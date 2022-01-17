d7b4c9974c0cd43c042c56db024356c7a242b80f
Daiz <taneli.vatanen@gmail.com>
2022-01-17 17:49:08 +0200

    Remove vite-tsconfig-paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { render } from "preact";
-import { Page } from "src/app";
-import "src/index.css";
+import { Page } from "./app";
+import "./index.css";
+import renderToString from "preact-render-to-string";
+
+/* this works too
+const html = renderToString(<Page />);
+document.getElementById("page")!.innerHTML = html;
+*/
 
 render(<Page />, document.getElementById("page")!);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 import ssr from "vite-plugin-ssr/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsConfigPaths(), preact(), ssr()],
+  plugins: [preact(), ssr()],
 });
